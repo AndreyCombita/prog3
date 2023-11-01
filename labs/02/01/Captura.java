5be1ebedc8136bb1b8779f81647bd513148a887e
AndreyCombita <125753783+AndreyCombita@users.noreply.github.com>
2023-10-31 23:40:54 -0500

    Add files via upload

--- labs/02/01/Captura.java
+++ labs/02/01/Captura.java
@@ -6,18 +6,18 @@ package Punto1;
 
 import java.util.Scanner;
 
-public class Captura {   // se recomienda guardarlo en un archivo .java separado
-          private Scanner leer = new Scanner(System.in);
+public class Captura {
+    private static Scanner leer = new Scanner(System.in);
 
-          public static double nextDouble(String message) {
-            System.out.print(message);
-            return leer.nextDouble(message);
-          }
+    public static double nextDouble(String message) {
+        System.out.print(message);
+        return leer.nextDouble();
+    }
 
-          public static double [][] readMat(int filas, int cols) {
-            double [][] matR = new double[filas][cols];
+    public static double[][] readMat(int filas, int cols) {
+        double[][] matR = new double[filas][cols];
 
-            // ... completar el desarrollo del método
-            return matR;
-          }
-        }
+        // ... completar el desarrollo del método
+        return matR;
+    }
+}
--- labs/02/01/Implementacion.java
+++ labs/02/01/Implementacion.java
@@ -1,12 +1,47 @@
 package Punto1;
 
+import static Punto1.Operacion.*;
+import java.util.Scanner;
+
 public class Implementacion {   // se recomienda guardarlo en un archivo .java separado
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-      System.out.println("\tSeleccione una opcion"
-      + "\n1.Suma de Matrices\n2.Producto de matrices\n3.Producto escalar de una matriz\n4.Transpuesta de una matriz");
-              
+        System.out.println("Ingrese el tamaño de las matrices (filas columnas): ");
+        int filas = scanner.nextInt();
+        int columnas = scanner.nextInt();
 
+        int[][] matriz1 = new int[filas][columnas];
+        int[][] matriz2 = new int[filas][columnas];
+
+        System.out.println("Ingrese los elementos de la primera matriz: ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz1[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Ingrese los elementos de la segunda matriz: ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz2[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("La suma de las matrices es: ");
+        imprimirMatriz(sumaMatrices(matriz1, matriz2));
+
+        System.out.println("El producto de las matrices es: ");
+        imprimirMatriz(productoMatrices(matriz1, matriz2));
+
+        System.out.println("Ingrese el escalar para multiplicar con la primera matriz: ");
+        int escalar = scanner.nextInt();
+
+        System.out.println("El producto del escalar con la primera matriz es: ");
+        imprimirMatriz(productoEscalarMatriz(escalar, matriz1));
+
+        System.out.println("La traspuesta de la primera matriz es: ");
+        imprimirMatriz(traspuestaMatriz(matriz1));
     }
 }
